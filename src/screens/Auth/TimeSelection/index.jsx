54eeb85b2e8ac1a70c98cf9df67d80946b036b83
Jackson-Wang-0 <97211608+Jackson-Wang-0@users.noreply.github.com>
2023-10-05 17:04:44 -0700

    Calendar
     select date and time functions

--- src/screens/Auth/TimeSelection/index.jsx
+++ src/screens/Auth/TimeSelection/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import {Text, View, SafeAreaView, ImageBackground} from 'react-native';
+import {Text, View, SafeAreaView, ImageBackground,TouchableOpacity} from 'react-native';
 import styles from './style';
 import LongBottom from '../../../components/LongButton';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import {Dimensions} from 'react-native';
 const {width, height} = Dimensions.get('window');
 import SwitchSelector from "react-native-switch-selector";
-import {useState} from 'react';
-import {Calendar, CalendarList, Agend} from 'react-native-calendars';
+import {useState, useEffect} from 'react';
+import {Calendar} from 'react-native-calendars';
+import DateTimePickerModal from "react-native-modal-datetime-picker";
 
 
 const TimeSelection = () => {
@@ -17,15 +18,41 @@ const TimeSelection = () => {
     { label: "PM", value: "2" },
   ];
   
-  
-  var hours = new Date().getHours(); //Current Hours
-  if (hours > 12) {
-    hours = hours - 12
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [selectedTime, setSelectedTime] = useState(null);
+  const [isTimePickerVisible, setTimePickerVisibility] = useState(false);
+  const [selectedAMPM, setSelectedAMPM] = useState("1"); // Default value "AM" with value "1"
+
+  const handleDayPress = (day) => {
+    setSelectedDate(day.dateString);
+  };
+
+  const markedDates = {};
+  if (selectedDate) {
+    markedDates[selectedDate] = { selected: true, selectedColor: 'blue' };
   }
-  console.log(hours)
-  var min = new Date().getMinutes(); //Current Minutes
-  
-    
+
+  const showTimePicker = () => {
+    setTimePickerVisibility(true);
+  };
+
+  const hideTimePicker = () => {
+    setTimePickerVisibility(false);
+  };
+
+  const handleTimeConfirm = (date) => {
+    setSelectedTime(date);
+    hideTimePicker();
+  };
+
+  const handleSubmit = () => {
+    console.log("Selected Date:", selectedDate);
+    console.log("Selected Time:", `${selectedTime.getHours()}:${selectedTime.getMinutes().toString().padStart(2, '0')}`);
+    console.log("AM/PM:", selectedAMPM);
+
+    // TODO send the data to an API.
+  };
+
   return (
     <SafeAreaView>
         <View style={styles.root}>
@@ -47,9 +74,8 @@ const TimeSelection = () => {
             
             <Calendar
                 style={{left: width * -0.015}}
-                onDayPress={day => {
-                console.log('selected day', day);
-                }}
+                onDayPress={handleDayPress}
+                markedDates={markedDates} // Set the markedDates prop to apply styling to selected dates
             />
             
             <View style={{flexDirection:"row", marginTop: 10}}>
@@ -58,30 +84,44 @@ const TimeSelection = () => {
                     <Text style={{fontSize: 20, fontWeight:"500"}}>Time</Text>
                 </View>
 
-                <ImageBackground 
-                source={gray_rec} 
-                style={{flex: 1, left: width * 0.1, height: 30, width: 60}}
-                imageStyle={{ borderRadius: 6}}>
-                    <Text style={{fontSize: 21, fontWeight:"500", left: 7}}>
-                        {min < 10 ? [hours, ":", 0, min] : [hours, ":", min]}
-                    </Text>
-                </ImageBackground>
+                <TouchableOpacity onPress={showTimePicker}>
+                    <ImageBackground 
+                    source={gray_rec} 
+                    style={{flex: 1, marginRight:10 ,height: 30, width: 65}}
+                    imageStyle={{ borderRadius: 6}}>
+                        <Text style={{fontSize: 21, fontWeight:"500", textAlign: "center"}}>
+                        {selectedTime
+                            ? `${selectedTime.getHours()}:${selectedTime.getMinutes().toString().padStart(2, '0')}`
+                            : `00:00`}
+                        </Text>
+                    </ImageBackground>
+                    <DateTimePickerModal
+                        isVisible={isTimePickerVisible}
+                        mode="time"
+                        onConfirm={handleTimeConfirm}
+                        onCancel={hideTimePicker}
+                        locale="en_GB"
+                    />
+                </TouchableOpacity>
+
 
                 <SwitchSelector
-                style={{flex: 1}}
-                borderRadius={10}
-                textColor={'#000000'}
-                selectedColor={'#e5e5e6'}
-                buttonColor={'#708090'}
-                height= {30}
-                options={options}
-                initial={0}
-                onPress={value => console.log(`Call onPress with value: ${value}`)}
+                    style={{ flex: 1 }}
+                    borderRadius={10}
+                    textColor={'#000000'}
+                    selectedColor={'#e5e5e6'}
+                    buttonColor={'#708090'}
+                    height={30}
+                    options={options}
+                    initial={selectedAMPM === "1" ? 0 : 1} // Initialize with the selectedAMPM state
+                    onPress={(value) => {
+                        setSelectedAMPM(value); // Update the selectedAMPM state when the user switches between AM and PM
+                    }}
                 />
             </View>
         </View>
 
-        <LongBottom otherStyle={styles.btn} text={'Submit'} onPress={() => {}} />
+        <LongBottom otherStyle={styles.btn} text={'Submit'} onPress={handleSubmit} />
         
         </View>
         
